60690475ed5766fa06e399604ee607b1e9857051
carloscarvallo <carloscarvallo87@gmail.com>
2017-07-13 22:54:48 -0400

    testing error treatments

--- index.js
+++ index.js
@@ -17,10 +17,11 @@ nunjucks.configure('assets', {
     express: app
 });
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3001;
 
 app.use(( req, res, next ) => {
   // some middleware
+  next();
 });
 
 app.get('/', ( req, res ) => {
@@ -32,13 +33,20 @@ app.get('/', ( req, res ) => {
 const routes = express.Router();
 
 routes.get('/', ( req, res ) => {
-    res.json({ msg: "Welcome to the API!" });
+    res.json({ msg: "Welcome to the Palindrome API!" });
 });
 
-routes.route('/test')
-    .get(( req, res ) => {
-      res.json({ msg: "I am users" });
-    });
+routes.route('/palindromes')
+  .get(( req, res ) => {
+    if (req.query.q)
+      res.send(req.query.q);
+    res.status(400).json({ error: "You must enter at least one palindrome" })
+  });
+
+routes.route('/palindromes/:palindrome')
+  .get(( req, res ) => {
+    res.send(req.params.palindrome);
+  });
 
 app.use('/api', routes);
 

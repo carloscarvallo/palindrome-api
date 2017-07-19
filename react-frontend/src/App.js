0dd7784618fb77645815caf0fce4663468807dbc
carloscarvallo <carloscarvallo87@gmail.com>
2017-07-19 00:18:05 -0400

    show component based on form submit

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import jQuery from 'jquery';
 
+function AlertLabel( props ) {
+  return <label className={`alert alert-dismissible ${props.typeAlert}`}>{props.message}</label>
+}
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       value : '',
-      message : ''
+      message : '',
+      showLabel : false,
+      typeAlert : null
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -19,31 +25,43 @@ class App extends Component {
   handleSubmit( event ) {
     jQuery.ajax({
       type: 'GET',
-      url: 'https://carloscarvallo.me/api/palindromes?q='+ this.state.value,
-      //url: 'http://localhost:3001/api/palindromes?q='+ this.state.value,
+      //url: 'https://carloscarvallo.me/api/palindromes?q='+ this.state.value,
+      url: 'http://localhost:3001/api/palindromes?q='+ this.state.value,
       success: function( data ) {
-        this.setState({ message: data.message });
+        this.setState({
+          message: data.message,
+          typeAlert: "alert-info"
+        });
       }.bind(this),
       error: function( err ) {
-        this.setState({ message: err.responseJSON.message });
+        this.setState({
+          message: err.responseJSON.message,
+          typeAlert: "alert-warning"
+        });
       }.bind(this)
     })
     event.preventDefault();
+    this.setState({ showLabel: true });
   }
 
   render() {
     return (
+      <div className="jumbotron container-fluid">
       <form onSubmit={this.handleSubmit}>
         <label>
-          Word:
+          Insert your Palidrome here!
+            <br/>
             <br/>
             <input type="text" value={this.state.value} onChange={this.handleChange} />
         </label>
         <br/>
+        <br/>
         <input type="submit" value="Submit" />
         <br/>
-        <label onChange={this.handleChange}>{this.state.message}</label>
+        <br/>
+        {this.state.showLabel ? <AlertLabel message={this.state.message} typeAlert={this.state.typeAlert} /> : null}
       </form>
+    </div>
     );
   }
 }
